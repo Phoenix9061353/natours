7c72c630f378335bf0e709d6eef89ba1684c78a6
Phoenix Chang <ph90630@gmail.com>
2022-11-12 00:09:21 +0800

    Fix little bugs

--- app.js
+++ app.js
@@ -60,7 +60,15 @@ const fontSrcUrls = [
   'fonts.gstatic.com',
   'cdnjs.cloudflare.com',
 ];
-
+if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https') {
+      res.redirect(`https://${req.header('host')}${req.url}`);
+    } else {
+      next();
+    }
+  });
+}
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
