a7ce69ec17522132f8526953475e81f93e5f01b1
Phoenix Chang <ph90630@gmail.com>
2022-11-12 17:50:53 +0800

    Solve little bugs

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -8,12 +8,12 @@ const {
   getMyTours,
 } = require('../controllers/viewController');
 const { isLoggedIn, protect } = require('../controllers/authController');
-const { createBookingCheckout } = require('../controllers/bookingController');
+// const { createBookingCheckout } = require('../controllers/bookingController');
 ///////////////////////////////////////
 const router = express.Router();
 // router.use(isLoggedIn);
 //避免isLoggedIn與protect的內容互衝
-router.get('/', createBookingCheckout, isLoggedIn, getOverview);
+router.get('/', isLoggedIn, getOverview);
 router.get('/tour/:tourSlug', isLoggedIn, getTour);
 router.get('/login', isLoggedIn, getLoginForm);
 router.get('/signup', isLoggedIn, getSignUpForm);
